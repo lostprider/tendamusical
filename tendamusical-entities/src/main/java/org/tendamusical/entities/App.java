8c9ce00411bded033442fa0da7531649073e1ad5
lostprider <albert.psahun@gmail.com>
2022-09-01 12:34:20 +0200

    Versio inicial del projecte final de tenda musical

---
+++ tendamusical-entities/src/main/java/org/tendamusical/entities/App.java
@@ -0,0 +1,13 @@
+package org.tendamusical.entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tendamusical-services/src/main/java/org/tendamusical/services/App.java
@@ -0,0 +1,13 @@
+package org.tendamusical.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
